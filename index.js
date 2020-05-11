21384f77a32e6347466bcc17217397ea70da691d
Ankit Mishra <ankitxvii@gmail.com>
2020-05-11 17:33:21 +0530

    updated mongoose with comment and rating

--- index.js
+++ index.js
@@ -17,10 +17,30 @@ connect.then((db) => {
        .then((dish) => {
             console.log(dish);
 
-            return Dishes.find({});
+            return Dishes.findByIdAndUpdate(dish._id, {
+
+                $set: {description: 'Updated test'},
+            },{
+                new: true
+            }).exec();
         })
-        .then((dishes) => {
-            console.log(dishes);
+
+
+
+        .then((dish) => {
+            console.log(dish);
+
+            dish.comments.push({
+                rating: 5,
+                comment: 'I\'m getting a sinking feeling',
+                author: 'Leonardo di carpaccio'
+            });
+
+            return dish.save();
+        })
+        .then((dish)=>{
+        console.log(dish);
+
 
             return Dishes.remove({});
         })
--- models/dishes.js
+++ models/dishes.js
@@ -1,6 +1,25 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const commentSchema = new Schema ({
+     rating: {
+         type: Number,
+         min: 1,
+         max: 5,
+         required: true  
+     },
+     comment :{
+         type : String,
+         required: true
+     },
+     author:{
+         type: String,
+         required: true
+     }
+    },{
+        timestamps: true
+});
+
 const dishSchema = new Schema({
     name: {
         type: String,
@@ -10,7 +29,9 @@ const dishSchema = new Schema({
     description: {
         type: String,
         required: true
-    }
+    },
+    comments:[commentSchema]
+
 },{
     timestamps: true  // this will automatically add the created at and updated at 2 timestamps into each
                    //document ie.stored in our application it will updated automatically these values whenever we update the document
